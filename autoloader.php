10fa552b3c6a3dd5273a10de88140a18f9f6030c
Joe Ponzio <joe@newnine.com>
2017-05-11 20:18:13 -0400

    File Exists check in autoloader

--- autoloader.php
+++ autoloader.php
@@ -8,6 +8,8 @@ spl_autoload_register( function( $class ){
 	$class_pieces = explode( '\\', $class );
 	$file = array_pop( $class_pieces );
 	$path = FLEXMLS_PLUGIN_DIR_PATH . '/' . implode( '/', $class_pieces ) . '/' . $file . '.php';
-	require_once( $path );
+	if( file_exists( $path ) ){
+		require_once( $path );
+	}
 
 } );
